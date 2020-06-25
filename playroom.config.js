9db5c517c1cbd18b765752067c0a470a9f5a2877
Dominik Ferber <dominik.ferber@gmail.com>
2020-06-25 19:55:09 +0200

    init

---
+++ playroom.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  components: "./components/index.ts",
+  snippets: "./snippets/index.ts",
+  outputPath: "./dist",
+  openBrowser: false,
+  webpackConfig: () => ({
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          include: __dirname,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-typescript",
+                "@babel/preset-react",
+              ],
+            },
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".ts", ".tsx"],
+    },
+  }),
+};
